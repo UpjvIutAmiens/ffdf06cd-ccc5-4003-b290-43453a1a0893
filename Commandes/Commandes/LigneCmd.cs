a08b2187ff490ae91ac7b28884c80c56dc433e13
jpdemory <jpdemory@manastria.fr>
2018-01-30 15:13:57 +0100

    Ajout de la classe LigneCmd avec champs et Description

---
+++ Commandes/Commandes/LigneCmd.cs
@@ -0,0 +1,14 @@
+﻿namespace Commandes
+{
+    class LigneCmd // Nom complet de la classe Commandes.LigneCmd
+    {
+        public string designation;
+        public double prixUnitaire;
+        public double quantite;
+
+        public string Description()
+        {
+            return $"{designation}, pu:{prixUnitaire}, qt:{quantite}";
+        }
+    }
+}
--- Commandes/Commandes/Program.cs
+++ Commandes/Commandes/Program.cs
@@ -10,6 +10,19 @@ namespace Commandes
     {
         static void Main(string[] args)
         {
+            // Creation d'une référence l1 qui peut pointer un objet LigneCmd
+            LigneCmd l1;
+
+            // Création d'un objet LigneCmd référencé par l1
+            l1 = new LigneCmd();
+
+            l1.designation = "Gomme";
+            l1.prixUnitaire = 1.3;
+            l1.quantite = 10;
+
+            Console.WriteLine("L1 : " + l1.Description());
+            Console.WriteLine("Press a key");
+            Console.ReadKey();
         }
     }
 }
