ac7620c4c1e5ee5cee89a903978192483b2b66b8
jpdemory <jpdemory@manastria.fr>
2018-02-01 08:44:17 +0100

    Creation de l'objet LigneCmd

---
+++ Commande/Commande/LigneCmd.cs
@@ -0,0 +1,15 @@
+﻿namespace Commande
+{
+    class LigneCmd // Nom complet de la classe Commande.LigneCmd
+    {
+        public string designation;
+        public double prixUnitaire;
+        public double quantite;
+
+        public string Description()
+        {
+            return
+                $"{designation} pu:{prixUnitaire}, qt:{quantite}";
+        }
+    }
+}
---
+++ Commande/Commande/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Commande
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Déclaration d'une référence capable de pointer vers un
+            // objet de type LigneCmd
+            LigneCmd l1;
+
+            // Création d'un objet LigneCmd avec l'opérateur new et stockage
+            // de la référence dans l1
+            l1 = new LigneCmd();
+
+            l1.designation = "gomme";
+            l1.prixUnitaire = 1.3;
+            l1.quantite = 10;
+
+            Console.WriteLine(l1.Description());
+        }
+    }
+}
