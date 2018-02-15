8bec76f4eca4d2279dbad9954dcb321faa2da7dc
jpdemory <jpdemory@manastria.fr>
2018-02-15 09:18:27 +0100

    Ajout de l'énoncé de complexe

---
+++ Complexe/Complexe/Complexe.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Text;
+
+namespace Complexe
+{
+    class Complexe
+    {
+        private double _partieReelle;                                  //partie réelle
+        private double _partieImaginaire;                              //partie imaginaire
+
+        public double PartieReelle
+        {
+            get { return _partieReelle; }
+            set { _partieReelle = value; }
+        }
+
+        public double PartieImaginaire
+        {
+            get { return _partieImaginaire; }
+            set { _partieImaginaire = value; }
+        }
+
+        public Complexe() { }
+
+        public Complexe(double partieReelle, double partieImaginaire)
+        {
+            this._partieReelle = partieReelle;
+        }
+
+        public Complexe Plus(Complexe c)               //La méthode retourne un objet (référence)
+        {
+            double relle, imaginaire;
+            relle = this._partieReelle + c._partieReelle;
+            imaginaire = this._partieImaginaire + c._partieImaginaire;
+
+            Complexe rs = new Complexe(relle, imaginaire);
+            return ;
+        }
+
+        public Complexe Moins(Complexe c)                //La méthode retourne un objet (référence)
+        {
+
+        }
+
+        public string Afficher()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            if (_partieImaginaire == 0)
+                sb.Append(_partieReelle);
+            else if (_partieImaginaire > 0)
+            {
+                if (_partieImaginaire == 1)
+                    sb.Append(_partieReelle + "+i");
+                else
+                    sb.Append(_partieReelle + "+" + _partieImaginaire + "i");
+            }
+            else
+            {
+                if (_partieImaginaire == -1)
+                    sb.Append(_partieReelle + "-i");
+                else
+                    sb.Append(_partieReelle + "" + _partieImaginaire + "i");
+            }
+
+            return sb.ToString();
+        }
+    }
+
+}
---
+++ Complexe/Complexe/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Complexe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Complexe c1 = new Complexe(2, 4);
+            Console.WriteLine("Nombre Complexe 1: ");
+            Console.WriteLine("  Partie réelle : {0}", c1.PartieReelle);
+            Console.WriteLine("  Partie imaginaire : {0}", c1.PartieImaginaire);
+            Console.WriteLine("  Nombre Complexe : {0}", c1.Afficher());
+
+            Console.Out.WriteLine("\n");
+            Complexe c2 = new Complexe();
+            c2.PartieReelle = 3;
+            c2.PartieImaginaire = -2;
+            Console.WriteLine("Nombre Complexe 2: ");
+            Console.WriteLine("  Partie réelle : {0}", c2.PartieReelle);
+            Console.WriteLine("  Partie imaginaire : {0}", c2.PartieImaginaire);
+            Console.WriteLine("  Nombre Complexe : {0}", c2.Afficher());
+
+
+            Console.Out.WriteLine("\n");
+            Console.Out.WriteLine("La somme: ");
+            Complexe c3 = c1.Plus();      //L'objet C3 est construit et sa référence est retournée par la méthode Plus
+            Console.WriteLine(c3.Afficher());
+
+
+            Console.Out.WriteLine("\n");
+            Console.Out.WriteLine("La Différence: ");
+            Complexe C4 = c1.Moins(c2);
+            Console.WriteLine(C4.Afficher());
+
+
+            Console.WriteLine("Appuyer sur une touche pour terminer");
+            Console.ReadKey();
+
+        }
+    }
+
+}
